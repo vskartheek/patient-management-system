c93128c3bec523d059ee93ab17e0f4b10b11c0d1
vskartheek <kartheek5050450@gmail.com>
2026-03-06 11:55:46 +0530

    created grpc client in patient service and tested the connection between client and grpc server

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,9 +1,11 @@
 package com.pm.patientservice.service;
 
+import billing.BillingResponse;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -21,6 +23,9 @@ public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private BillingServiceGrpcClient billingServiceGrpcClient;
+
 
     public List<PatientResponseDTO> getAllPatients() {
         List<Patient> patients = patientRepository.findAll();
@@ -31,6 +36,8 @@ public class PatientService {
         if (!patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
             Patient patient = PatientMapper.convertToEntity(patientRequestDTO);
             Patient savedPatient = patientRepository.save(patient);
+
+            billingServiceGrpcClient.createBillingAccount(savedPatient.getId().toString(),savedPatient.getName(),savedPatient.getEmail());
             return PatientMapper.convertToDTO(savedPatient);
         } else {
             throw new EmailAlreadyExistsException("Email already exists: " + patientRequestDTO.getEmail());
@@ -61,4 +68,5 @@ public class PatientService {
             throw new PatientNotFoundException("Patient not found with id: " + id);
         }
     }
+
 }
