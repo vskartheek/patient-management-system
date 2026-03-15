26b2d13d389a54d1907e9e62bba107763368c4f7
vskartheek <kartheek5050450@gmail.com>
2026-03-15 15:30:57 +0530

    created patient-event proto file for kafka

---
+++ patient-service/src/main/java/com/pm/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,17 @@
+package com.pm.patientservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class KafkaProducer {
+
+
+    private KafkaTemplate<String,byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+}
